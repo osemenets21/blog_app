7bfa3197558fad26c6b8d14cd5ef63538fea10f7
artem <csharper89@gmail.com>
2024-09-28 15:58:36 +0300

    fix

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/register", {
+      const response = await fetch("http://localhost:5000/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
